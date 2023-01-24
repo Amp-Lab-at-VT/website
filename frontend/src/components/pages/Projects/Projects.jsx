37f1d886ca41639758c93c2284c3f730f33def02
Forsyth-Creations <robert.h.forsyth@gmail.com>
2023-01-24 14:34:47 -0500

    Projects yaml

--- frontend/src/components/pages/Projects/Projects.jsx
+++ frontend/src/components/pages/Projects/Projects.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import ReactMarkdown from 'react-markdown'
+import YAML from 'yaml'
 
 import Navigation from "../../modules/Nav/Nav.jsx"
 
@@ -8,12 +9,20 @@ import "./projects.css"
 
 export default function Projects() {
   const url =
-    "https://raw.githubusercontent.com/Amp-Lab-at-VT/AmpWebV2/master/repos.md";
+    "https://raw.githubusercontent.com/Amp-Lab-at-VT/AmpWebV2/master/repos.yaml";
   const [read, setRead] = useState(null);
+  const [importedYaml, setImportedYaml] = useState(null);
+  const [projects, setProjects] = useState([]);
+
 
   useEffect(() => {
     axios.get(url).then((response) => {
       setRead(response.data);
+      var items = YAML.parseDocument(response.data).contents.items;
+      console.log(items)
+      for (var item in items){
+        console.log(item.key.value)
+      }
     });
   }, []);
 
@@ -23,7 +32,7 @@ export default function Projects() {
         <Navigation></Navigation>
         <text>Projects</text>
         <div className="App">
-          <ReactMarkdown>{read}</ReactMarkdown>
+          <ReactMarkdown>{YAML.stringify(importedYaml)}</ReactMarkdown>
         </div>
       </header>
     </div>
