c4d2350b45beff7d173f36992b104510c372454e
Forsyth-Creations <robert.h.forsyth@gmail.com>
2023-04-17 00:28:13 -0400

    Latest images:

--- src/pages/Leadership.jsx
+++ src/pages/Leadership.jsx
@@ -2,14 +2,14 @@
 import React from "react";
 
 import LeadershipBox from "@/comps/LeadershipBox/LeadershipBox.jsx"
-import "@/pages/Leadership/leadership.module.css"
-
 import Baumann from "../../public/Headshots/Baumann.jpg"
 import Gibbons from "../../public/Headshots/Gibbons.jpg"
 import Forsyth from "../../public/Headshots/Forsyth2.jpg"
 import Meadows from "../../public/Headshots/Meadows.jpg"
 import Pritchard from "../../public/Headshots/Pritchard.jpeg"
 import Blank from "../../public/Headshots/blank.jpeg"
+import Ethan from "../../public/Headshots/Ethan.jpg"
+import PB from "../../public/Headshots/pb.jpeg"
 
 export default function Leadership() {
 
@@ -23,10 +23,8 @@ export default function Leadership() {
           <LeadershipBox name = "Richard Gibbons" src={Gibbons} title = "Richard Gibbons: Team Lead" email = "gricha1@vt.edu"></LeadershipBox>
           <LeadershipBox name = "Henry Forsyth" src={Forsyth} title = "Henry Forsyth: Mentor/Webdev" email = "rhforsythjr@vt.edu"></LeadershipBox>
           <LeadershipBox name = "Eddie Pritchard" src={Pritchard} title = "Eddie Pritchard: Mentor" email = "epritchard@vt.edu"></LeadershipBox>
-          <LeadershipBox name = "Purv Bavishi" src={Blank} title = "Purv Bavishi: Mentor" email = "purvbavishi@vt.edu"></LeadershipBox>
-          <LeadershipBox name = "Tyler Wells" src={Blank} title = "Tyler Wells: Mentor" email = "twells@vt.edu"></LeadershipBox>
-          <LeadershipBox name = "Alex Something" src={Blank} title = "Alex Something: Mentor" email = "alex@vt.edu"></LeadershipBox>
-
+          <LeadershipBox name = "Purv Bavishi" src={PB} title = "Purv Bavishi: Mentor" email = "purvbavishi@vt.edu"></LeadershipBox>
+          <LeadershipBox name = "Ethan James" src={Ethan} title = "Ethan James: Mentor" email = "ethanjamesauto@vt.edu"></LeadershipBox>
         </div>
         <h2>Meet Our Team: Professors</h2>
         <hr></hr>
