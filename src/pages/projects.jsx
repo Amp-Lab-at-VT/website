e7111768abe44436f7c0ace769af885507dc3577
HullabalooRobotics <33364183+HullabalooRobotics@users.noreply.github.com>
2023-04-30 23:22:51 -0400

    Cleanup

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -17,7 +17,7 @@ export default function Projects({ activeProjects, inactiveProjects }) {
       <SearchBar setExternalSearchTerm={setSearchTerm} setExternalFilterType={setFilterType} />
       {/* Green bar that says active */}
       <div className="flex flex-wrap justify-center">
-      {searchTerm || filterType ? <div></div> : <div className="w-full h-20 bg-green-500 p-2"><h1>Active Projects</h1></div>}
+      {searchTerm || filterType ? <div></div> : <div className="w-screen h-20 bg-green-500 p-2"><h1>Active Projects</h1></div>}
         {
           Object.keys(activeProjects).map((key) => {
             if (searchTerm == "" && filterType == "") {
@@ -61,7 +61,7 @@ export default function Projects({ activeProjects, inactiveProjects }) {
       </div>
       {/* Inactive Project Section */}
       {(searchTerm || filterType) && !inactiveProjects ? <div></div> :     
-      <div className="w-full h-20 bg-red-500 p-2">
+      <div className="w-100% h-20 bg-red-500 p-2">
       <h1>Inactive Projects (No commits in 90 days)</h1>
       </div>}
 
@@ -79,7 +79,7 @@ export default function Projects({ activeProjects, inactiveProjects }) {
         }
         else
         {
-          return (<p>No inactive projects</p>);
+          return (<p key={key}>No inactive projects</p>);
         }
       })}
     </div>
