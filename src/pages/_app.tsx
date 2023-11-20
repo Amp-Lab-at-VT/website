e0b360235b02c051084977fde0c8e65b8ee76d98
Forsyth Creations <robert.h.forsyth@gmail.com>
2023-11-20 13:00:46 -0500

    Rules (#127)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,27 @@
-import '@/styles/globals.css'
-import Navigation from "@/comps/Nav/Nav.jsx"
-import Footer from "@/comps/Footer/Footer.jsx"
+import '@/styles/globals.css';
+import Navigation from '@/comps/Nav/Nav.jsx';
+import Footer from '@/comps/Footer/Footer.jsx';
 
-import type { AppProps } from 'next/app'
+// import image
+import Image from 'next/image';
 
-import Box from '@mui/material/Box';
+import type { AppProps } from 'next/app';
+
+import { Box } from '@mui/material';
+
+import background from '../images/cover.jpeg';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Box>
-      <Navigation />
-      <Component {...pageProps} sx={{minHeight : "100vh"}}/>
-      <Footer />
+    <Box sx={{ position: 'relative', overflow: 'hidden', minHeight: '100vh' }}>
+      <Box sx={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 1, opacity: 0.25}}>
+        <Image src={background} alt="background" layout="fill" objectFit="cover"/>
+      </Box>
+      <Box sx={{ position: 'relative', zIndex: 1 }}>
+        <Navigation />
+        <Component {...pageProps} />
+        <Footer />
+      </Box>
     </Box>
-  )
+  );
 }
---
+++ src/pages/rules.jsx
@@ -0,0 +1,21 @@
+//https://react-icons.github.io/react-icons/icons?name=bs
+import { promises as fs } from 'fs'
+import ReactMarkdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
+
+export default function Rules({fileContents}) {
+  return (
+    <div>
+        <div class = "App-pageHelper">
+        <ReactMarkdown class="App-standardPage" rehypePlugins={[rehypeRaw]}>{fileContents}</ReactMarkdown>
+        </div> 
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const file = 'rules.md'
+  const fileContents = await fs.readFile( process.cwd() + '/markdowns/' + file, 'utf8')
+  return {props: {fileContents}}
+}
+
