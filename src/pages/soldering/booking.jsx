acc7b9b34bb0a62245d0aad7cbef28c1e6972ae1
HullabalooRobotics <33364183+HullabalooRobotics@users.noreply.github.com>
2024-01-06 14:20:02 -0500

    Booking on website

---
+++ src/pages/soldering/booking.jsx
@@ -0,0 +1,18 @@
+// Create an iframe for a booking calendar
+
+import React from "react"
+
+import {Container, Stack} from "@mui/material"
+
+
+export default function Booking() {
+    return (
+        <Container >
+            <Stack justifyContent="center">
+                <iframe style= {{ height: "90vh", border: "none"}}
+                    src='https://outlook.office365.com/owa/calendar/SolderingTrainings@VirginiaTech.onmicrosoft.com/bookings/'
+                ></iframe>
+            </Stack>
+        </Container>
+    )
+    }
