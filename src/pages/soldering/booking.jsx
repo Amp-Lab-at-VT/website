3217868665dc9e05ee9013fb383299548a273f19
HullabalooRobotics <33364183+HullabalooRobotics@users.noreply.github.com>
2024-01-07 16:19:24 -0500

    Warning

--- src/pages/soldering/booking.jsx
+++ src/pages/soldering/booking.jsx
@@ -1,18 +1,26 @@
 // Create an iframe for a booking calendar
 
-import React from "react";
+import React from "react"
+import {Container, Stack, Alert} from "@mui/material"
+
+const src='https://outlook.office365.com/owa/calendar/SolderingTrainings@VirginiaTech.onmicrosoft.com/bookings/'
+
 
 import { Container, Stack } from "@mui/material";
 
 export default function Booking() {
-  return (
-    <Container>
-      <Stack justifyContent="center">
-        <iframe
-          style={{ height: "90vh", border: "none" }}
-          src="https://outlook.office365.com/owa/calendar/SolderingTrainings@VirginiaTech.onmicrosoft.com/bookings/"
-        ></iframe>
-      </Stack>
-    </Container>
-  );
-}
+
+    // todo: attempt to connect to microsoft account
+    // if not connected, display alert
+
+    return (
+        <Container >
+            <Stack justifyContent="center">
+                <Alert severity  = "warning">You must be logged into your Microsoft account to access. If the application doesn't load, please log into your Microsoft account separately and return to this page</Alert>
+                <iframe style= {{ height: "90vh", border: "none"}}
+                src={src}
+                ></iframe>
+            </Stack>
+        </Container>
+    )
+    }
