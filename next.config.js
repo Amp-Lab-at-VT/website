9e10c8e2ad087a8e7f8e1388abe5c861b22cc2b9
Saksham Goyal <sakgoy2001@vt.edu>
2024-02-19 01:10:28 +0000

    hotfix

--- next.config.js
+++ next.config.js
@@ -1,13 +1,18 @@
-module.exports = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "raw.githubusercontent.com",
-				port: "",
-				pathname: "/**",
-			},
-		],
-		unoptimized: true,
-	},
-};
+// @ts-check
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "raw.githubusercontent.com",
+                port: "",
+                pathname: "/**",
+            },
+        ],
+        unoptimized: true,
+    },
+    output : 'export',
+}
+
+module.exports = nextConfig;
