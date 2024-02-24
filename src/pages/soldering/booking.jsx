3fad9df51d7379642d9994065657e8738de6c9d2
Forsyth Creations <robert.h.forsyth@gmail.com>
2024-02-24 10:13:01 -0500

    Bookings fix (#148)

--- src/pages/soldering/booking.jsx
+++ src/pages/soldering/booking.jsx
@@ -1,7 +1,7 @@
 // Create an iframe for a booking calendar
 
 import React from "react";
-import { Container, Stack, Alert } from "@mui/material";
+import { Container, Stack, Alert, Button } from "@mui/material";
 
 const src =
   "https://outlook.office365.com/owa/calendar/SolderingTrainings@VirginiaTech.onmicrosoft.com/bookings/";
@@ -17,6 +17,10 @@ export default function Booking() {
           You must be logged into your Microsoft account to access. If the
           application doesn't load, please log into your Microsoft account
           separately and return to this page
+
+          <Button href={src}>
+            Click here if the application doesn't load
+          </Button>
         </Alert>
         <iframe style={{ height: "90vh", border: "none" }} src={src}></iframe>
       </Stack>
