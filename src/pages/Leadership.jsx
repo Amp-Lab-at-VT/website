43050dccc8bd0b4aaad398bafa2bc7efe35751e6
niujijun143 <njijun143@gmail.com>
2025-09-16 21:27:10 -0400

    alumni section added

--- src/pages/Leadership.jsx
+++ src/pages/Leadership.jsx
@@ -33,43 +33,48 @@ function Leadership() {
           title="Lab Safety Lead/Web Maintain"
           email="njijun24@vt.edu"
           />
-          <LeadershipBox
-          name="Tyler Wells"
-          src={Wells}
-          title="Former Lab Lead"
-          email="tylermwells@vt.edu"
-          />
-          <LeadershipBox
-            name="Henry Forsyth"
-            src={Forsyth}
-            title="Mentor/Webdev"
-            email="rhforsythjr@vt.edu"
-          ></LeadershipBox>
-          <LeadershipBox
-            name="Eddie Pritchard"
-            src={Pritchard}
-            title="Former Lab Lead/Mentor"
-            email="epritchard@vt.edu"
-          ></LeadershipBox>
-          <LeadershipBox
-            name="Purv Bavishi"
-            src={PB}
-            title="Souldering Trainer/Mentor"
-            email="purvbavishi@vt.edu"
-          ></LeadershipBox>
-          <LeadershipBox
-            name="Ethan James"
-            src={Ethan}
-            title="Mentor"
-            email="ethanjamesauto@vt.edu"
-          ></LeadershipBox>
-            <LeadershipBox
-            name="Yassin Lahrime"
-            src={Yassin}
-            title="Mentor"
-            email="yassinl@vt.edu"
-          ></LeadershipBox>
+
       </Grid>
+
+
+<Typography variant="h4" gutterBottom sx={{ marginTop: 4 }}>
+  Meet Our Team: Student Alumni
+</Typography>
+<Divider />
+<Grid container spacing={3} sx={{ marginTop: 2 }}>
+<LeadershipBox
+    name="Tyler Wells"
+    src={Wells}
+    title="Lab Lead"
+    email="tylermwells@vt.edu"
+    />
+    <LeadershipBox
+      name="Henry Forsyth"
+      src={Forsyth}
+      title="Lab Lead/Webdev"
+      email="rhforsythjr@vt.edu"
+    ></LeadershipBox>
+    <LeadershipBox
+      name="Eddie Pritchard"
+      src={Pritchard}
+      title="Mentor"
+      email="epritchard@vt.edu"
+    ></LeadershipBox>
+    <LeadershipBox
+      name="Purv Bavishi"
+      src={PB}
+      title="Souldering Trainer/Mentor"
+      email="purvbavishi@vt.edu"
+    ></LeadershipBox>
+    <LeadershipBox
+      name="Ethan James"
+      src={Ethan}
+      title="Mentor"
+      email="ethanjamesauto@vt.edu"
+    ></LeadershipBox>
+
+</Grid>
+
 
       <Typography variant="h4" gutterBottom sx={{ marginTop: 4 }}>
         Meet Our Team: Professors
