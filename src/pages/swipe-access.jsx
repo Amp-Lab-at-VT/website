c1ccca5a95a91fe32fea2b0b38ede18a77f1ff77
daniel-cherkasskiy <cherkasskiyd@vt.edu>
2026-09-03 21:14:42 -0400

    Revise swipe access page for AMP members
    Updated the content and structure of the swipe access page to reflect new guidelines and document links for AMP members. Adjusted alert messages and document titles for clarity.

--- src/pages/swipe-access.jsx
+++ src/pages/swipe-access.jsx
@@ -4,7 +4,6 @@ import {
   Box,
   Container,
   Stack,
-  Paper,
   Fade,
   Card,
   Button,
@@ -17,50 +16,57 @@ function Page() {
 
   return (
     <Container>
-      <div class="App-pageHelper">
-        <Typography variant="h4">
-          Documents for Students using the ECE Design Studio
+      <div className="App-pageHelper">
+        <Typography variant="h4" sx={{ mb: 2 }}>
+          ECE Design Studio Swipe Access
         </Typography>
+
         <Alert sx={{ marginBottom: "10px" }} severity="info">
-          The documents on this page are utilized to obtain swipe access to AMP
-          Lab (Whittemore 264), MDE Lab (Whittemore 264) and/or Robotics Lab
-          (Durham 373). To obtain swipe access to these labs, download and
-          adhere to requirements outlined in the Lab Training Guide. ECE Lab
-          Policy and ECE Lab Waiver are support documents called out in the Lab
-          Training Guide.
+          These documents are used by AMP members requesting swipe access to
+          the ECE Integrated Design Studio in Whittemore 234. Follow the AMP
+          Swipe Access Guide and complete all required training and
+          documentation before submitting your access request.
         </Alert>
-        <Alert sx={{ marginBottom: "10px" }} severity="info">
-          Note: These documents are universal to both the MDE and AMP lab sides
-          of the lab, but they do NOT grant you access to both sides of the lab.
-          You will need to be apart of the AMP lab to use their side
+
+        <Alert sx={{ marginBottom: "20px" }} severity="info">
+          The ECE Design Studio contains separate MDE and AMP sides. Swipe
+          access to Whittemore 234 does not automatically authorize use of both
+          sides of the lab. AMP resources are reserved for active AMP members
+          working on approved AMP projects.
         </Alert>
+
         <Stack>
           <SingleDoc
-            title="Welcome to the AMP LAB. You are probably here to request access to the AMP LAB. "
-            description="Follow the Lab Training and Swipe Access Guide below"
-            link={`https://github.com/Amp-Lab-at-VT/website/raw/${branchName}/docs/mde_docs/Lab%20Training%20Guide%20Rev%202.docx`}
-            last_updated="1/16/2024"
+            title="AMP Swipe Access Guide"
+            description="Follow this guide to complete the requirements for AMP swipe access to Whittemore 234."
+            link={`https://github.com/Amp-Lab-at-VT/website/raw/${branchName}/docs/mde_docs/Request%20Swipe%20Access.docx`}
+            last_updated="9/3/2026"
           />
+
           <SingleDoc
-            title="ECE Lab Policy"
-            description="This contains the policy for the ECE Lab Policy."
+            title="ECE Design Studio Policy"
+            description="Read the ECE Design Studio policies, safety requirements, and rules for use of the MDE and AMP sides of the lab."
             link={`https://github.com/Amp-Lab-at-VT/website/raw/${branchName}/docs/mde_docs/ECE%20Design%20Studio%20Policy.docx`}
-            last_updated="1/5/2024"
+            last_updated="9/3/2026"
           />
+
           <SingleDoc
-            title="ECE Lab Waiver"
+            title="ECE Integrated Design Studio Waiver"
             description={
               <Box>
-                <Alert severity="warning">
-                  This is the document you will submit for swipe access
+                <Alert severity="warning" sx={{ mt: 1, mb: 1 }}>
+                  This waiver must be completed and submitted as part of the AMP
+                  onboarding process.
                 </Alert>
+
                 <Typography>
-                  This contains the waiver for the ECE Lab
+                  Read, complete, and sign this waiver before requesting swipe
+                  access.
                 </Typography>
               </Box>
             }
             link={`https://github.com/Amp-Lab-at-VT/website/raw/${branchName}/docs/mde_docs/ECE%20Integrated%20Design%20Studio%20Waiver.docx`}
-            last_updated="1/5/2024"
+            last_updated="9/3/2026"
           />
         </Stack>
       </div>
@@ -79,14 +85,19 @@ function SingleDoc(props) {
             display: "flex",
             flexDirection: "row",
             justifyContent: "space-between",
+            alignItems: "flex-start",
+            gap: 2,
           }}
         >
           <Typography variant="h6">{title}</Typography>
-          <Typography variant="caption">
+
+          <Typography variant="caption" sx={{ whiteSpace: "nowrap" }}>
             Last Updated: {last_updated}
           </Typography>
         </Box>
-        <Box>{description}</Box>
+
+        <Box sx={{ mt: 1, mb: 1 }}>{description}</Box>
+
         <Button href={link}>Download Document</Button>
       </Card>
     </Fade>
